600a710bc66bd17aa97348ad52d3a2529a402a3c
MacBook <jim.casey321@gmail.com>
2017-03-21 22:41:42 +0200

    Add script

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
-function Artist(n, l, b, d) {
-this.name = n;
-this.lastName = l;
-this.birthDate = b;
-this.deathDate = d;
-this.century = function() {
-  if (this.deathDate > 1801 && this.deathDate < 1901) {
-      console.log("19th Century.");
-  } else if (this.deathDate > 1901 && this.deathDate < 2001) {
-      console.log("20th Century.");
-  } else {
-      console.log('<br/>');
-  }
+
+  var counter = 0;
+  function addParagraph() {
+
+      var div = document.getElementById('add');
+
+      if (counter == 2) {
+          var listParagraph = document.getElementsByTagName('p');
+          for (var i = 0; i < listParagraph.length +1; i++) {
+              console.log(div.firstChild);
+              div.removeChild(div.firstChild);
+          }
+
+          counter = 0;
+          return;
+      }
+
+
+      var paragraph = document.createElement('p');
+      paragraph.innerHTML = 'new paragraph';
+      div.appendChild(paragraph);
+
+      counter = counter + 1;
+
 }
-}
-
-Artist.prototype.showCentury = function() {
-console.log(this.century() + '<br/>')
-};
-
-Artist.prototype.showArtist = function() {
-console.log(this.name + ' ' + this.lastName + ' was born in ' + this.birthDate + '. Died in ' + this.deathDate +'. Art movement: '+ this.artMovement +'<br/>');
-};
-
-Artist.prototype.artMovement = "Art Movement";
-
-var vanGogh = new Artist('Vincent', 'van Gogh', 1853, 1890);
-var picasso = new Artist('Pablo', 'Picasso', 1881, 1973);
-var kahlo = new Artist('Frida', 'Kahlo', 1907, 1954);
-
-
-vanGogh.artMovement = "Post-Impressionism";
-picasso.artMovement = "Cubism, Surrealism";
-kahlo.artMovement = "Surrealism, Magic realism";
-
-vanGogh.showCentury();
-vanGogh.showArtist();
-
-picasso.showCentury();
-picasso.showArtist();
-
-kahlo.showCentury();
-kahlo.showArtist();
