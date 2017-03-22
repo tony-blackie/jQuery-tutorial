06dc83f860800fd7cdf9f3bd305d87f2cceaf8b7
Anton Spaskyi <asp@kyiv.ciklum.net>
2017-03-22 16:40:04 +0200

    change to interval task

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-function Artist(n, l, b, d) {
-this.name = n;
-this.lastName = l;
-this.birthDate = b;
-this.deathDate = d;
-this.century = function() {
-  if (this.deathDate > 1801 && this.deathDate < 1901) {
-      console.log("19th Century.");
-  } else if (this.deathDate > 1901 && this.deathDate < 2001) {
-      console.log("20th Century.");
-  } else {
-      console.log('<br/>');
-  }
+window.onload = function() {
+    var second = 1; // counter
+    var intervalHandler, intervalHandlerM, intervalHandlerH;
+    var minute = 0;
+    var hour = 0;
+
+
+    function countSecond() {
+        get('stopwatch').innerHTML = hour +":" + minute + ":" + second;
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    }
+
+    get('start').onclick = function() {
+        intervalHandler = setInterval(countSecond, 1000);
+    }
+
+    get('stop').onclick = function() {
+        clearInterval(intervalHandler);
+    }
+
+    get('reset').onclick = function() {
+        get('stopwatch').innerHTML = 0 + ':' + 0;
+    }
 }
+
+var get = function (id){
+    return document.getElementById(id);
 }
-
-Artist.prototype.showCentury = function() {
-console.log(this.century() + '<br/>')
-};
-
-Artist.prototype.showArtist = function() {
-console.log(this.name + ' ' + this.lastName + ' was born in ' + this.birthDate + '. Died in ' + this.deathDate +'. Art movement: '+ this.artMovement +'<br/>');
-};
-
-Artist.prototype.artMovement = "Art Movement";
-
-var vanGogh = new Artist('Vincent', 'van Gogh', 1853, 1890);
-var picasso = new Artist('Pablo', 'Picasso', 1881, 1973);
-var kahlo = new Artist('Frida', 'Kahlo', 1907, 1954);
-
-
-vanGogh.artMovement = "Post-Impressionism";
-picasso.artMovement = "Cubism, Surrealism";
-kahlo.artMovement = "Surrealism, Magic realism";
-
-vanGogh.showCentury();
-vanGogh.showArtist();
-
-picasso.showCentury();
-picasso.showArtist();
-
-kahlo.showCentury();
-kahlo.showArtist();
